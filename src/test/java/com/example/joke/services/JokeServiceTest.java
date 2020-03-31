dbeb6c392cde24c936aa18b60385f4a5ad31e124
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2020-03-30 18:21:22 -0700

    added new jokes, refactored list usage

--- src/test/java/com/example/joke/services/JokeServiceTest.java
+++ src/test/java/com/example/joke/services/JokeServiceTest.java
@@ -31,9 +31,12 @@ class JokeServiceTest {
     private Joke joke1;
     private Joke joke2;
     private Joke joke3;
+    private Joke joke4;
+    private Joke joke5;
+    private Joke joke6;
     private String jokeContent;
     private JokeType jokeType;
-
+    private List<Joke> jokeList;
 
     //CREATE
 
@@ -56,15 +59,36 @@ class JokeServiceTest {
                 "but when I do, it shuts our production environment down.\" ;) ";
         jokeType = JokeType.TECHNOLOGY;
         joke3 = new Joke(jokeType, jokeContent);
+        jokeContent = "How do you make holy water? You boil the hell out of it.";
+        jokeType = JokeType.DADJOKES;
+        joke4 = new Joke(jokeType, jokeContent);
+        jokeContent = "Why is a computer so smart?Because it listens to its motherboard.";
+        jokeType = JokeType.MOMJOKES;
+        joke5 = new Joke(jokeType, jokeContent);
+        jokeContent = "What's the difference between a motorcycle and a vacuum cleaner. One can fit two dirt bags on it.";
+        jokeType = JokeType.KIDJOKES;
+        joke6 = new Joke(jokeType, jokeContent);
         //Exercise
         Joke actual1 = jokeService.save(joke1);
         Joke actual2 = jokeService.save(joke2);
         Joke actual3 = jokeService.save(joke3);
+        Joke actual4 = jokeService.save(joke4);
+        Joke actual5 = jokeService.save(joke5);
+        Joke actual6 = jokeService.save(joke6);
+        jokeList = new ArrayList<>();
+        jokeList.add(joke1);
+        jokeList.add(joke2);
+        jokeList.add(joke3);
+        jokeList.add(joke4);
+        jokeList.add(joke5);
+        jokeList.add(joke6);
         //Assert
         assertEquals(joke1.hashCode(), actual1.hashCode());
         assertEquals(joke2.hashCode(), actual2.hashCode());
         assertEquals(joke3.hashCode(), actual3.hashCode());
-
+        assertEquals(joke4.hashCode(), actual4.hashCode());
+        assertEquals(joke5.hashCode(), actual5.hashCode());
+        assertEquals(joke6.hashCode(), actual6.hashCode());
     }
 
 
@@ -74,10 +98,7 @@ class JokeServiceTest {
     @Test
     public void findAll_daoWithExistingJokes_returnsJokeList() {
         //Setup
-        List<Joke> expected = new ArrayList<>();
-        expected.add(joke1);
-        expected.add(joke2);
-        expected.add(joke3);
+        List<Joke> expected = jokeList;
         //Exercise
         List<Joke> actual = jokeService.findAll();
         //Assert
